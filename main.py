f355b79837595338ef62dd18d53f43d7d84526b3
Dmitry <sgysh3nka@ya.ru>
2024-09-22 14:38:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,32 +5,22 @@ def credits():
   print("Coder: sgysh3nka")
   webbrowser.open("https://github.com/sgysh3nka")
 
-def custom_dice():
-  firnum = input('First number? ')
-  secnum = input('Second number? ')
-  custom_dice228 = random.randiant(firnum, secnum)
-  print(custom_dice228)
-
 def dice():
   random_dice = random.randinat(1, 31)
   print(random_dice)
 
 def vibor():
   print("1. Dice.")
-  print("2. Custom dice.")
-  print("3. Credits.")
+  print("2. Credits.")
   vibor228 = input('')
   if vibor228 == "1":
-    dice():
-    vibor():
+    dice()
+    vibor()
   elif vibor228 == "2":
-    custom_dice():
-    vibor():
-  elif vibor228 == "3":
-    credits():
-    vibor():
+    credits()
+    vibor()
   else:
     print("404")
-    vibor():
+    vibor()
 
-vibor():
+vibor()
